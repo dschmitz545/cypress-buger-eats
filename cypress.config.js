dc153cc344e9726bf8255dc748f9ced108f0b492
Diego Schmitz <diego.sctar@gmail.com>
2022-09-01 17:39:14 -0300

    refatoração, criado page object, separada massa de teste, criado hooks

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
-module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+const { defineConfig } = require('cypress');
+
+
+module.exports = defineConfig(
+  {
+    e2e: {
+      baseUrl: 'https://buger-eats.vercel.app',
+      viewportWidth: 1440, viewportHeight: 900,
     },
-  },
-};
+  }); 
---
+++ cypress/pages/SignupPage.js
@@ -0,0 +1,48 @@
+class SignupPage {
+    go() {
+        cy.viewport(1024, 768)
+        cy.visit("/")
+
+        cy.get('#page-home .content a[href="/deliver"]').click()
+        cy.get("#page-deliver h1").should("have.text", "Cadastre-se para  fazer entregas")
+    }
+
+    fillForm(deliver) {
+
+        cy.get('#page-deliver input[placeholder="Nome completo"]').type(deliver.name)
+        cy.get('#page-deliver input[name="cpf"]').type(deliver.cpf)
+        cy.get('#page-deliver input[type="email"]').type(deliver.email)
+        cy.get('#page-deliver input[placeholder="Whatsapp"]').type(deliver.whatsapp)
+
+        cy.get('#page-deliver .field-group input[name="postalcode"]').type(deliver.address.postalcode)
+        cy.get('#page-deliver .field input[type="button"][value="Buscar CEP"]').click()
+
+        cy.get('#page-deliver .field input[name="address-number"][placeholder="Número"]').type(deliver.address.number)
+        cy.get('#page-deliver .field input[name="address-details"][placeholder="Complemento"]').type(deliver.address.details)
+
+        cy.get('#page-deliver .field input[name="address"][placeholder="Rua"]').should("have.value", deliver.address.street)
+        cy.get('#page-deliver .field input[name="district"][placeholder="Bairro"]').should("have.value", deliver.address.district)
+        cy.get('#page-deliver .field input[name="city-uf"][placeholder="Cidade/UF"]').should("have.value", deliver.address.city_state)
+
+        cy.contains('.delivery-method li', deliver.delivery_method).click();
+
+        cy.get('.dropzone input[accept^="image"][type="file"]').selectFile(deliver.cnh, { force: true });
+
+    }
+
+    submit() {
+        cy.get('#page-deliver .button-success[type="submit"]').click();
+    }
+
+    modalContentShouldBe() {
+
+        cy.get('.swal2-container div[class="swal2-html-container"]').should("have.text", "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.")
+
+    }
+
+    alertMessageShouldBe(expectMessage) {
+        cy.get('.field-group .field .alert-error').should("have.text", expectMessage)
+    }
+
+}
+export default SignupPage;

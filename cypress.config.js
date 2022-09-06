580d0b05d860f8afcde15b8558639a0e8e5f850a
Diego Schmitz <diego.sctar@gmail.com>
2022-09-06 14:05:23 -0300

    ajustes do ambiente gitactions

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig(
   {
     e2e: {
+      projectId: "y2243j",
       baseUrl: 'https://buger-eats-qa.vercel.app',
       viewportWidth: 1440, viewportHeight: 900,
     },
